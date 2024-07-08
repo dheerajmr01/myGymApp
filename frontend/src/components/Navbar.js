2435b5b3387f5177a8c57dfccc948332b49efb78
Dheeraj <dheerajmr01@gmail.com>
2024-07-08 23:30:45 +0530

    base

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,39 @@
+import { Link } from "react-router-dom"
+import { useAuthContext } from '../hooks/useAuthContext'
+import { useLogout } from '../hooks/useLogout'
+
+const Navbar = () => {
+    const { logout } = useLogout()
+    const { user } = useAuthContext()
+    
+    
+    const handleClick = () => { 
+        logout();
+    }
+    return (
+        <header>
+            <div className="container">
+                <Link to="/">
+                    <h1>Workout Tracker</h1>
+                </Link>
+                <nav>
+                    {user && (
+                    <div>
+                        <span>{ user.email }</span>
+                        <button onClick={handleClick}>Log Out</button>
+                    </div>
+                    )}
+                    {!user && (
+                        <div>
+                            <Link to="/signup">Signup</Link>
+                            <Link to="/login">Login</Link>
+                        </div>
+                    )}    
+                </nav>
+
+            </div>
+       </header>
+   ) 
+}
+
+export default Navbar
