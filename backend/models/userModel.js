775f6edd7671967a74e06f73a5652539fd8c9cc8
Dheeraj <dheerajmr01@gmail.com>
2024-07-15 16:36:19 +0530

    username feature

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,72 +1,78 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-const validator = require('validator');
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const validator = require("validator");
 
-const Schema = mongoose.Schema
+const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true,
-    }
-})
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
 
 //static signup methodd
 
-userSchema.statics.signup = async function(email, password) {
-    
-    //validation
-    if (!email || !password) { 
-        throw Error("Invalid email or password(Cannot be empty)");
-    } 
-    if (!validator.isEmail(email)) { 
-        throw Error("Invalid email");
-    }
-    if (!validator.isStrongPassword(password)) {
-        throw Error("Invalid password");
-    }
+userSchema.statics.signup = async function (email, username, password) {
+  //validation
+  if (!email || !password || !username) {
+    throw Error("Invalid email/username or password (Cannot be empty)");
+  }
+  if (!validator.isEmail(email)) {
+    throw Error("Invalid email");
+  }
+  if (!validator.isStrongPassword(password)) {
+    throw Error("Invalid password");
+  }
 
-    const exists = await this.findOne({ email })
+  const existsEmail = await this.findOne({ email });
+  const existsUsername = await this.findOne({ email });
 
-    if (exists) {
-        throw Error('Email already exists')
-    }
+  if (existsEmail) {
+    throw Error("Email already exists");
+  }
+  if (existsUsername) {
+    throw Error("Username already exists");
+  }
 
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, salt);
 
-    const salt = await bcrypt.genSalt(10)
-    const hash = await bcrypt.hash(password, salt)
+  const user = await this.create({ email, password: hash });
 
-    const user = await this.create({ email, password: hash })
-    
-
-    return user
-}
+  return user;
+};
 
 //static login method
 
-userSchema.statics.login = async function (email, password) { 
-    if (!email || !password) { 
-        throw Error("Invalid email or password(Cannot be empty)");
-    } 
+userSchema.statics.login = async function (email, password) {
+  if (!email || !password) {
+    throw Error("Invalid email or password(Cannot be empty)");
+  }
 
-    const user = await this.findOne({ email })
+  const user = await this.findOne({ email });
 
-    if (!user) {
-        throw Error('Email incorrect!')
-    }
+  if (!user) {
+    throw Error("Email incorrect!");
+  }
 
-    const match = await bcrypt.compare(password, user.password)
+  const match = await bcrypt.compare(password, user.password);
 
-    if (!match) {
-        throw Error("Invalid password"); 
-    }
+  if (!match) {
+    throw Error("Invalid password");
+  }
 
-    return user;
-}
+  return user;
+};
 
-module.exports = mongoose.model('User', userSchema)
+module.exports = mongoose.model("User", userSchema);
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,39 +1,37 @@
-import { Link } from "react-router-dom"
-import { useAuthContext } from '../hooks/useAuthContext'
-import { useLogout } from '../hooks/useLogout'
+import { Link } from "react-router-dom";
+import { useAuthContext } from "../hooks/useAuthContext";
+import { useLogout } from "../hooks/useLogout";
 
 const Navbar = () => {
-    const { logout } = useLogout()
-    const { user } = useAuthContext()
-    
-    
-    const handleClick = () => { 
-        logout();
-    }
-    return (
-        <header>
-            <div className="container">
-                <Link to="/">
-                    <h1>Workout Tracker</h1>
-                </Link>
-                <nav>
-                    {user && (
-                    <div>
-                        <span>{ user.email }</span>
-                        <button onClick={handleClick}>Log Out</button>
-                    </div>
-                    )}
-                    {!user && (
-                        <div>
-                            <Link to="/signup">Signup</Link>
-                            <Link to="/login">Login</Link>
-                        </div>
-                    )}    
-                </nav>
+  const { logout } = useLogout();
+  const { user } = useAuthContext();
 
+  const handleClick = () => {
+    logout();
+  };
+  return (
+    <header>
+      <div className="container">
+        <Link to="/">
+          <h1>Workout Tracker</h1>
+        </Link>
+        <nav>
+          {user && (
+            <div>
+              <span>{user.username}</span>
+              <button onClick={handleClick}>Log Out</button>
             </div>
-       </header>
-   ) 
-}
+          )}
+          {!user && (
+            <div>
+              <Link to="/signup">Signup</Link>
+              <Link to="/login">Login</Link>
+            </div>
+          )}
+        </nav>
+      </div>
+    </header>
+  );
+};
 
-export default Navbar
+export default Navbar;
